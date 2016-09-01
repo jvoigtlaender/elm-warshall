72ba2f89c6195f34b511bd1077e83081cc003202
jvoigtlaender <jvoigtlaender@users.noreply.github.com>
2016-09-01 20:14:24 +0200

    initial commit

---
+++ src/Warshall.elm
@@ -0,0 +1,40 @@
+module Warshall (warshall) where
+
+{-| The Warshall Algorithm, on type `Matrix` from the
+[`eeue56/elm-flat-matrix` package](http://package.elm-lang.org/packages/eeue56/elm-flat-matrix/latest).
+
+@docs warshall
+-}
+
+import Matrix exposing (Matrix)
+import Maybe
+
+
+connectedIn : Matrix Bool -> Int -> Int -> Bool
+connectedIn matrix i j =
+    Maybe.withDefault False (Matrix.get i j matrix)
+
+
+{-| `warshall 4` on matrix
+
+    [ [ False, True,  False, False ]
+    , [ False, False, True,  False ]
+    , [ False, False, False, True  ]
+    , [ False, False, False, False ]
+    ]
+
+gives matrix
+
+    [ [ False, True,  True,  True  ]
+    , [ False, False, True,  True  ]
+    , [ False, False, False, True  ]
+    , [ False, False, False, False ]
+    ]
+-}
+warshall : Int -> Matrix Bool -> Matrix Bool
+warshall n matrix =
+    let
+        step k matrix =
+            Matrix.indexedMap (\i j t -> t || connectedIn matrix i k && connectedIn matrix k j) matrix
+    in
+        List.foldl step matrix [0..n - 1]
